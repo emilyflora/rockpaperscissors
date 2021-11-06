6b393d14a00a0fac1704e7551a849d74a2ec06b2
emilyflora <e.kyne@outlook.com>
2021-11-06 12:29:17 -0500

    Added button functionality

--- script.js
+++ script.js
@@ -1,57 +1,68 @@
+let playerPoints=0;
+let computerPoints=0;
+
+var rock = document.querySelector("#rock");
+var paper = document.querySelector("#paper");
+var scissors = document.querySelector("#scissors");
+
+rock.addEventListener("click", playRound);
+paper.addEventListener("click", playRound);
+scissors.addEventListener("click", playRound);
+
+
 function computerPlay(){
     const options = ['rock', 'paper', 'scissors'];
     return options[Math.floor(Math.random() * options.length)];
 }
 
-function playRound(){
+function playRound(e){
     computerPlay();
     let computerSelection = computerPlay();
-    let playerSelection = prompt("Make a selection:", "rock, paper, or scissors").toLowerCase();
-    if (computerSelection == "rock") {
-        switch(playerSelection) {
-            case "rock": return("It's a tie!");
-                break;
-            case "paper": playerPoints++; 
-            return("You win! Paper beats rock."); 
-                break;
-            case "scissors": computerPoints++;
-            return("You lose! Rock beats scissors.");
-                break;}}
-    else if (computerSelection == "paper") {
-        switch(playerSelection) {
-            case "rock": computerPoints++;
-            return("You lose! Paper beats rock.");
-                break;
-            case "paper": return("It's a tie!");
-                break;
-            case "scissors": playerPoints++;
-            return("You win! Scissors beats paper.");
-                break;}}
-    else {
-        switch(playerSelection) {
-            case "rock": playerPoints++; 
-            return("You win! Rock beats scissors.");
+    console.log(computerSelection);
+    let playerSelection = e.target.id;
+    console.log(playerSelection);
+    if (playerSelection == "rock") {
+        switch(computerSelection) {
+            case "rock": console.log("It's a tie!");
                 break;
             case "paper": computerPoints++; 
-            return("You lose! Scissors beats paper.");
+            console.log("You lose! Paper beats rock."); 
                 break;
-            case "scissors": return("It's a tie!");
+            case "scissors": playerPoints++;
+            console.log("You win! Rock beats scissors.");
+                break;}}
+    else if (playerSelection == "paper") {
+        switch(computerSelection) {
+            case "rock": playerPoints++;
+            console.log("You win! Paper beats rock.");
+                break;
+            case "paper": console.log("It's a tie!");
+                break;
+            case "scissors": computerPoints++;
+            console.log("You lose! Scissors beats paper.");
+                break;}}
+    else {
+        switch(computerSelection) {
+            case "rock": computerPoints++; 
+            console.log("You lose! Rock beats scissors.");
+                break;
+            case "paper": playerPoints++; 
+            console.log("You win! Scissors beats paper.");
+                break;
+            case "scissors": console.log("It's a tie!");
                 break;}}
 }
 
-let playerPoints=0;
-let computerPoints=0;
+// function game() {
+//     while (playerPoints<=4&&computerPoints<=4){
+//         playRound();
+//     }
+//     if (playerPoints == 5) {
+//         return("You win! Refresh to play again.");
+//     }
+//     else {
+//         return("You lose! Refresh to play again.");
+//     }
+// }
 
-function game() {
-    while (playerPoints<=4&&computerPoints<=4){
-        playRound();
-    }
-    if (playerPoints == 5) {
-        return("You win! Refresh to play again.");
-    }
-    else {
-        return("You lose! Refresh to play again.");
-    }
-}
-
-    game();
+// game();
